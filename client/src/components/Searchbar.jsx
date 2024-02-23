e1f860519e7632f21381e6c38b7c678f67f95cc1
Sauby <imsauby@gmail.com>
2024-02-23 11:18:00 +0530

    created searchbar and filter

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
 
-const Searchbar = () => {
+// eslint-disable-next-line react/prop-types
+const Searchbar = ({ handleAddTask, handleSetFilter }) => {
+  const [task, setTask] = useState("");
+
+  const handleSubmit = () => {
+    if (task.length !== 0) handleAddTask(task);
+  };
+
   return (
     <div>
-        <div>Searchbar</div>
-    </div>
-  )
-}
+      <form className="add-task" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add a todo..."
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
 
-export default Searchbar
+      <div className="filter">
+        <select
+          name="filter"
+          id="filter"
+          onChange={(e) => handleSetFilter(e.target.value)}
+        >
+          <option value="all">All</option>
+          <option value="done">Done</option>
+          <option value="pending">Pending</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Searchbar;
